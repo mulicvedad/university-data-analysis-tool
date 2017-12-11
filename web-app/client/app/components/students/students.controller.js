1e2c85a4469e4010430e4b5b28463d81ac3f19bb
mulicvedad <mulic.vedad@gmail.com>
2017-12-11 12:36:13 +0100

    prikaz informacija o upisu

--- web-app/client/app/components/students/students.controller.js
+++ web-app/client/app/components/students/students.controller.js
@@ -4,13 +4,11 @@ class StudentsController {
  constructor(studentService) {
         this.studentService = studentService;
         this.currentAcademicYear = 2017;
-        this.setupEnrollmentChart();
-        this.loadData();
+        this.setupEnrollmentCharts();
+        //this.loadData();
     }
 
-    setupEnrollmentChart() {
-        this.labels = ["Liberal Arts", "Human P.", "Finance", "Business M.", "Computer Science"];
-        this.series = ['Redovni'];
+    setupEnrollmentCharts() {
         this.options = {
             scales: {
               yAxes: [
@@ -23,47 +21,103 @@ class StudentsController {
               ]
             }
         };
-        this.data = [
-            35, 20, 17, 16, 13
-        ];
-        this.data2 = [
-            20, 25, 17, 15, 13
-        ];
-        this.data3 = [
-            14, 20, 30, 15, 13
-        ];
-        this.data4 = [
-            35, 20, 17, 15, 34
-        ];
+        this.setupEnrollmentByAcademicYear();
+        this.setupEnrollmentDepartment();
+        this.setupEnrollmentByBudget();
+        this.setupEnrollmentByRepeating();
+    }
 
-
+    setupEnrollmentByAcademicYear() {
+        this.labelsAY = ["2014", "2015", "2016", "2017"];
+        this.seriesAY = ['Series'];
         
+        this.data = [
+            35, 20, 17, 16
+        ];
+    }
+
+    setupEnrollmentDepartment() {
+        this.labelsDep = ["RI", "AiE", "EE", "TK"];
+        this.seriesDep = ['Series'];
+    
+        this.data2 = [
+            35, 20, 17, 15
+        ];
+    }
+
+    setupEnrollmentByBudget() {
+        this.labelsBudget = ["Redovni", "Samofinansirajuci"];
+        this.seriesBudget = ['Series'];
+        
+        this.data3 = [
+            20, 25
+        ];
+    }
+
+    setupEnrollmentByRepeating() {
+        this.labelsRepeating = ["Redovni", "Ponovci"];
+        this.seriesRepeating = ['Series'];
+        
+        this.data4 = [
+            14, 20
+        ];
     }
 
     loadData() {
-        this.loadEnrollmentData();
+        this.loadEnrollmentByAY();
+        this.loadEnrollmentByDepartment();
+        this.loadEnrollmentByBudget();
+        this.loadEnrollmentByRepeating();
     }
 
-    loadEnrollmentData(numYears = 5) {
+    loadEnrollmentByAY(numYears = 4) {
         var tmp = 0;
         for (var i = 0; i < numYears; i++) { 
             tmp = this.currentAcademicYear - numYears + 1 + i;
             this.enrollmentForYear(i, tmp);
-            this.registeredStudentsForYear(i,tmp);
         }   
     }
-    
-    enrollmentForYear(idx, year) {
-        this.studentService.enrollmentReport(year).then((response) => {
-            this.data[idx] = response.data; 
+
+    loadEnrollmentByDepartment(numDeps = 4) {
+        for (var i = 1; i <= numDeps; i++) { 
+            this.studentService.enrollmentByDepartment(i + 8).then((response) => {
+                console.log("vrijednost od i: " + i);
+                this.data2[i - 1] = response.data;
+            }, (error) => {
+                console.log("Greska: " + error);
+            });
+        }   
+    }
+
+    loadEnrollmentByBudget() {
+        this.studentService.enrollmentByBudget(1).then((response) => {
+            this.data3[0] = response.data;
+        }, (error) => {
+            console.log("Greska: " + error);
+        });
+        this.studentService.enrollmentByBudget(0).then((response) => {
+            this.data3[1] = response.data;
         }, (error) => {
             console.log("Greska: " + error);
         });
     }
-    registeredStudentsForYear(idx, year)
-    {
-        this.studentService.registeredStudentsReport(year).then((response) => {
-            this.data2[idx] = response.data;     
+    
+    loadEnrollmentByRepeating() {
+        this.studentService.enrollmentByRepeating(0).then((response) => {
+            this.data4[0] = response.data;
+        }, (error) => {
+            console.log("Greska: " + error);
+        });
+        this.studentService.enrollmentByRepeating(0).then((response) => {
+            this.data4[1] = response.data;
+        }, (error) => {
+            console.log("Greska: " + error);
+        });
+    }
+
+    enrollmentForYear(idx, year) {
+        this.studentService.enrollmentByAcademicYear(year).then((response) => {
+            this.data[idx] = response.data; 
         }, (error) => {
             console.log("Greska: " + error);
         });
