e84fe40713d349443aea2e048794bcf49e2627c4
mulicvedad <mulic.vedad@gmail.com>
2017-12-21 19:13:46 +0100

    Prikaz informacija o tabelama na frontendu

--- web-app/client/app/components/dataImport/dataImport.controller.js
+++ web-app/client/app/components/dataImport/dataImport.controller.js
@@ -4,7 +4,11 @@ class DataImportController {
     constructor(importDataService, swalService) {
         this.importDataService = importDataService;
         this.swalService = swalService;
+        this.allTablesInfo = [{tableName: "Departments", dateDW: "22-12-2017", dateRDB: "21-12-2017", isUpToDate: true}, 
+            {tableName: "Courses", dateDW: "22-12-2017", dateRDB: "21-12-2017", isUpToDate: true},
+            {tableName: "Teachers", dateDW: "22-12-2017", dateRDB: "21-12-2017", isUpToDate: true}  ];
         $("#spinner-div").hide();
+        this.loadData();
     }
 
     importData() {
@@ -16,6 +20,18 @@ class DataImportController {
             $("#spinner-div").hide();            
             this.swalService.displayError("Doslo je do greske prilikom ucitavanja podataka: \n" + error.message);
         });
+    }
+
+    loadData() {
+        this.loadTablesInfo();
+    }
+
+    loadTablesInfo() {
+        this.importDataService.allTablesInfo().then((response) => {
+            this.allTablesInfo = response.data;
+        }).catch((error) => {
+            console.log("Error in 'loadTablesInfo': " + error);
+        })
     }
 }
 
